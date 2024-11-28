8e82385ba6f49e55d79988f80223f33cab2a656d
Bruce Axtens <bruce.axtens@gmail.com>
2024-11-28 10:04:28 +0800

    initial

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+namespace wrapper;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length < 2) {
+            Console.WriteLine("wrapper.exe path_to_exe path_to_arguments");
+            return;
+        }
+        if (!File.Exists(args[0])) {
+            Console.WriteLine("path_to_exe file not found.");
+            return;
+        }
+        if (!File.Exists(args[1])) {
+            Console.WriteLine("path_to_arguments file not found.");
+            return;
+        }
+        string argumentList = string.Join(' ',File.ReadAllLines(args[1]).Select(x => "\"" + x.Trim() + "\"").ToArray());
+        ProcessStartInfo psi = new ProcessStartInfo(args[0]) {
+            UseShellExecute = false,
+            Arguments = argumentList
+        };
+        Console.WriteLine(argumentList);
+        Process.Start(psi);
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Wrapper
+
+execute parameter1 giving it the contents of parameter2 as if on a command line.
